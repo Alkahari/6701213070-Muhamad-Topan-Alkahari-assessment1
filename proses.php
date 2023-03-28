e8354573391622eb9c851482e053b4761ec825a1
Muhamad Topan Alkahari <topanalkahari@student.telkomuniversity.ac.id>
2023-03-28 11:11:52 +0700

    v1.2.1

---
+++ proses.php
@@ -0,0 +1,27 @@
+<?php
+if (isset($_POST['submit'])) {
+    $angka1 = $_POST['angka1'];
+    $angka2 = $_POST['angka2'];
+    $operator = $_POST['operator'];
+
+    switch ($operator) {
+        case "+":
+            $hasil = $angka1 + $angka2;
+            break;
+        case "-":
+            $hasil = $angka1 - $angka2;
+            break;
+        case "*":
+            $hasil = $angka1 * $angka2;
+            break;
+        case "/":
+            $hasil = $angka1 / $angka2;
+            break;
+        default:
+            echo "Operator tidak valid";
+            exit;
+    }
+
+    echo "Hasil perhitungan: " . $angka1 . " " . $operator . " " . $angka2 . " = " . $hasil;
+}
+?>
